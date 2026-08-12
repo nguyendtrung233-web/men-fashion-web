759c7cd4d16087b5a54f67598901a5e0dcaf3f74
nguyendtrung233-web <nguyendtrung233@gmail.com>
2026-08-12 14:21:25 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App.jsx';
+import App from './App.tsx';
 import './index.css';
 
 const rootElement = document.getElementById('root');
